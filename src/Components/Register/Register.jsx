c36e4b98e5ddb4e51e8fdb6ed79d9270790d2b4f
Ayan Sujon <ayansujonbd@gmail.com>
2025-11-01 00:25:57 +0600

    added user api

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -8,6 +8,24 @@ const handleGoogleSignIn = ()=> {
     signInWithGoogle()
     .then(result => {
         console.log(result.user);
+        const newUser = {
+            name: result.user.displayName,
+            email: result.user.email,
+            image: result.user.photoURL
+        }
+
+        // Create user in the database.
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        } )
+        .then( res => res.json())
+        .then(data => {
+            console.log("Data after user save", data);
+        })
     })
     .catch(error => {
         console.log(error)
